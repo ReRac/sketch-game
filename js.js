ecfeba542e513bd51377f3ff56c7bea2b8e179ea
Reda <reda.rackauskaite@gmail.com>
2023-05-09 00:00:30 +0300

    Add visualisation to grid

--- js.js
+++ js.js
@@ -12,33 +12,8 @@ document.body.appendChild(grid);
 div1.appendChild(grid);
 grid.setAttribute("id", "grid");
 
-let input = 257;
+// update with invalid input option
 
-function createGrid(input) {
-  for (i = 0; i < input; i++) {
-    let cell = document.createElement("div");
-    grid.appendChild(cell);
-    cell.classList.add(".cell");
-  }
+function grid = (choice = 16) {
+  for(let i = 0; i = choice * choice; i++);
 }
-createGrid(input);
-console.log(input);
-
-// create 16 blocks grid
-// let input = "";
-
-// for (let i = 1; i < 257; i++) {
-//   const div = document.createElement("td");
-//   // div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
-//   grid.appendChild(div);
-// }
-
-//Add class for grid
-
-// const elements = document.querySelectorAll("#grid *");
-
-// elements.forEach((element) => {
-//   element.classList.add("cell");
-// });
-
-// //hover pc

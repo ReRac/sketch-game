3c3ab0dde94792f88c2a39526430b8282a8dc43e
Reda <reda.rackauskaite@gmail.com>
2023-05-12 11:53:52 +0300

    Create a grid

--- js.js
+++ js.js
@@ -2,14 +2,22 @@ const resetBtn = document.querySelector(".reset");
 const colorBtn = document.querySelector(".color");
 const monochromaticBtn = document.querySelector(".monochromatic");
 const submitBtn = document.querySelector(".submit");
-const gridB = document.querySelector("#grid-box");
+const container = document.querySelector("#container");
+const gridContainer = document.querySelector(".grid");
 
-function makeGrid(n = 16) {
-  // created div for the element:
-  const gridDiv = document.createElement("div");
-  gridDiv.classList.add("make-div");
-  gridB.appendChild(gridDiv);
-  let cell = document.createElement("div");
-  cell.classList.add("cell");
-  gridDiv.appendChild(cell);
-}
+let size = 4;
+
+const createGrid = (number) => {
+  for (let i = 0; i < number; i++) {
+    let rows = document.createElement("div");
+    rows.classList.add("rows");
+
+    for (let j = 0; j < number; j++) {
+      let gridBox = document.createElement("div");
+      gridBox.classList.add("grid-box");
+      rows.appendChild(gridBox);
+    }
+    gridContainer.appendChild(rows);
+  }
+};
+createGrid(size);

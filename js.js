8c74e0419c984cf40ce7ac3ebc49b3233677e124
Reda <reda.rackauskaite@gmail.com>
2023-05-16 13:16:27 +0300

    Create slider input for grid

--- js.js
+++ js.js
@@ -4,8 +4,9 @@ const monochromaticBtn = document.querySelector(".monochromatic");
 const submitBtn = document.querySelector(".submit");
 const container = document.querySelector("#container");
 const gridContainer = document.querySelector(".grid");
+const rangeInput = document.querySelector(".range-size");
 
-let size = 15;
+// Create gird, loop creates row and loops same time creating additional rows
 const createGrid = (number) => {
   const alphabet = "FOOD";
   for (let i = 0; i < number; i++) {
@@ -16,10 +17,9 @@ const createGrid = (number) => {
       let gridBox = document.createElement("div");
       gridBox.classList.add("grid-box");
       rows.appendChild(gridBox);
-      if (size === 15) {
+      if (range === 15) {
         let randomLetter =
           alphabet[Math.floor(Math.random() * alphabet.length)];
-        alphabet.style;
         gridBox.textContent = randomLetter;
       }
     }
@@ -27,45 +27,12 @@ const createGrid = (number) => {
   }
 };
 
-createGrid(size);
+//Slider function witch removes old data when using it
+rangeInput.addEventListener("input", function () {
+  const rangeValue = rangeInput.value;
+  //remove created grid before new one
+  gridContainer.innerHTML = "";
+  createGrid(parseInt(rangeValue));
+});
 
-//grid size determined by user input
-
-// let getInput = prompt("Enter a whole number between 1 and 100");
-// getInput = createElement();
-// //grid creation
-
-// function createElement(n = 16) {
-//   // alter an existing elements of gridB
-//   gridB.innerHTML = "";
-//   // css properties
-//   document.documentElement.style.setProperty("--grid-cols", n);
-//   document.documentElement.style.setProperty("--grid-rows", n);
-//   // create the boxes and add them to gridB
-//   for (let i = 0; i < n * n; i++) {
-//     let box = document.createElement("div");
-//     box.id = "box" + i;
-//     box.classList = "boxes";
-//     gridB.appendChild(box);
-//   }
-// }
-// update with invalid input option
-
-// RETURN FUNCTION WITH LETTER
-// const createGrid = (number) => {
-//   const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//   for (let i = 0; i < number; i++) {
-//     let rows = document.createElement("div");
-//     rows.classList.add("rows");
-
-//     for (let j = 0; j < number; j++) {
-//       let gridBox = document.createElement("div");
-//       gridBox.classList.add("grid-box");
-//       let randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-//       gridBox.textContent = randomLetter;
-//       rows.appendChild(gridBox);
-//       console.log(randomLetter);
-//     }
-//     gridContainer.appendChild(rows);
-//   }
-// };
+createGrid(15);

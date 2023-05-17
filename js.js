74388126f2089b1b873ee06fb87d0b2e9ad80026
Reda <reda.rackauskaite@gmail.com>
2023-05-17 15:14:43 +0300

    Create color button color picking and impying color on chosen divs

--- js.js
+++ js.js
@@ -1,11 +1,12 @@
 const resetBtn = document.querySelector(".reset");
 const colorBtn = document.querySelector(".color");
-const monochromaticBtn = document.querySelector(".monochromatic");
+const blackAndWhiteBtn = document.querySelector(".monochromatic");
 const submitBtn = document.querySelector(".submit");
 const container = document.querySelector("#container");
 const gridContainer = document.querySelector(".grid");
 const rangeInput = document.querySelector(".range-size");
-
+const colorPicker = document.querySelector(".color-pick");
+const gridBox = document.querySelector(".grid-box");
 // Create gird, loop creates row and loops same time creating additional rows
 const createGrid = (number) => {
   const alphabet = "FOOD";
@@ -17,7 +18,7 @@ const createGrid = (number) => {
       let gridBox = document.createElement("div");
       gridBox.classList.add("grid-box");
       rows.appendChild(gridBox);
-      if (number === 15) {
+      if (number === 8) {
         let randomLetter =
           alphabet[Math.floor(Math.random() * alphabet.length)];
         gridBox.textContent = randomLetter;
@@ -34,5 +35,17 @@ rangeInput.addEventListener("input", function () {
   gridContainer.innerHTML = "";
   createGrid(parseInt(rangeValue));
 });
-
 createGrid(15);
+
+colorBtn.addEventListener("click", () => {
+  let color = "green";
+  color = colorPicker.value;
+  console.log(color);
+
+  let gridBoxes = document.querySelectorAll(".grid-box");
+  for (let i = 0; i < gridBoxes.length; i++) {
+    gridBoxes[i].addEventListener("mouseover", () => {
+      gridBoxes[i].style.backgroundColor = color;
+    });
+  }
+});
